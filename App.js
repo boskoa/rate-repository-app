b56540e0fd708de342849a81a43822e1026ccddd
boskoa <bosko.ap@gmail.com>
2022-04-11 19:33:17 +0200

    Adding exercises 10.1.-10.2.

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('working...');
   return (
     <View style={styles.container}>
-      <Text>Ojsa!!!</Text>
+      <Text>Ojsa!!!1</Text>
       <StatusBar style="auto" />
     </View>
   );
